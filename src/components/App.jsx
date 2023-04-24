f9e9730fdf4020fe9c9a9698ce922f8ef7efde77
V_Borysenko <preinspiron@gmail.com>
2023-04-24 16:45:30 +0200

    task_1 Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import Profiles from './Prifiles/profile';
+import user from './user.json';
+// console.log(users);
+
 export const App = () => {
   return (
     <div
@@ -7,10 +11,16 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profiles
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
